ebb7822f6f97c9b7738cebed48395045f1f79073
lucaspintos06 <lpintos@ceibal.edu.uy>
2023-01-26 11:50:49 -0300

    feat: Agregando eventos al completar y borrar item

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -2,11 +2,25 @@ import React from "react";
 import "./TodoItem.css";
 
 function TodoItem({ text, completed }) {
+  const onComplete = () => {
+    alert("Completaste el todo " + text);
+  };
+
+  const onDelete = () => {
+    alert("Eliminaste el todo " + text);
+  };
+
   return (
     <li className={`todo_item ${completed && "todo_item_completed"}`}>
-      <span className="todo_item__icon">{completed ? "Done" : "To do"}</span>
+      <span className="todo_item__icon" onClick={onComplete}>
+        <button className="todo_item__icon_button">
+          {completed ? "Done" : "To do"}
+        </button>
+      </span>
       <p className="todo_item__text">{text}</p>
-      <span className="todo_item__btn">X</span>
+      <span className="todo_item__delete" onClick={onDelete}>
+        <button className="todo_item__delete_button">X</button>
+      </span>
     </li>
   );
 }

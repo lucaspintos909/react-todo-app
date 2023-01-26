3bb368c8f4b5b7cb6695d56858c17af5bc77fb3d
lucaspintos06 <lpintos@ceibal.edu.uy>
2023-01-26 17:04:54 -0300

    update: Moviendo lógica y estados a TodoContext

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,48 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
 import { CreateTodoButton } from "../components/CreateTodoButton/index.js";
 import { TodoCounter } from "../components/TodoCounter/index.js";
 import { TodoItem } from "../components/TodoItem/index.js";
 import { TodoList } from "../components/TodoList/index.js";
 import { TodoSearch } from "../components/TodoSearch/index.js";
+import { TodoContext } from "../context/TodoContext.js";
 
-function AppUI({
-  completedTodos,
-  totalTodos,
-  searchedTodos,
-  toggleTodoState,
-  onDeleteTodo,
-  setSearchValue,
-  searchValue,
-  loading,
-  error,
-}) {
+function AppUI() {
+  const { error, loading, searchedTodos, toggleTodoState, onDeleteTodo } =
+    useContext(TodoContext);
+
   return (
     <>
-      <>
-        <TodoCounter completedTodos={completedTodos} totalTodos={totalTodos} />
+      <TodoCounter />
 
-        <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
+      <TodoSearch />
 
-        <TodoList>
-          {error && <p>Hubo un error, recargue la página por favor.</p>}
-          {loading && <p>Cargando...</p>}
-          {!loading && !searchedTodos.length && <p>No hay todos</p>}
+      <TodoList>
+        {error && <p>Hubo un error, recargue la página por favor.</p>}
+        {loading && <p>Cargando...</p>}
+        {!loading && !searchedTodos.length && <p>No hay todos</p>}
 
-          {!loading &&
-            !error &&
-            searchedTodos.map((todo) => (
-              <TodoItem
-                key={todo.id}
-                text={todo.text}
-                completed={todo.completed}
-                toggleTodoState={() => toggleTodoState(todo.id)}
-                onDeleteTodo={() => onDeleteTodo(todo.id)}
-              />
-            ))}
-        </TodoList>
+        {!loading &&
+          !error &&
+          searchedTodos.map((todo) => (
+            <TodoItem
+              key={todo.id}
+              text={todo.text}
+              completed={todo.completed}
+              toggleTodoState={() => toggleTodoState(todo.id)}
+              onDeleteTodo={() => onDeleteTodo(todo.id)}
+            />
+          ))}
+      </TodoList>
 
-        <CreateTodoButton />
-      </>
+      <CreateTodoButton />
     </>
   );
 }
---
+++ src/context/TodoContext.js
@@ -0,0 +1,72 @@
+import { createContext, useState } from "react";
+import { getTodoIndexById } from "../components/utils/getTodoIndexById";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+
+const VERSION = "TODOS_V1";
+
+const TodoContext = createContext();
+
+function TodoProvider(props) {
+  const {
+    item: todos,
+    saveItem: saveTodos,
+    loading,
+    error,
+  } = useLocalStorage(VERSION, []);
+
+  const [searchValue, setSearchValue] = useState("");
+
+  const completedTodos = todos.filter((todo) => !!todo.completed).length;
+  const totalTodos = todos.length;
+
+  let searchedTodos = [];
+
+  if (searchValue.length === 0) {
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter((todo) =>
+      todo.text.toLowerCase().includes(searchValue.toLowerCase())
+    );
+  }
+
+  const toggleTodoState = (id) => {
+    const todoIndex = getTodoIndexById(id, todos);
+
+    if (todoIndex !== false) {
+      let newTodos = [...todos];
+      newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
+      saveTodos(newTodos);
+    }
+  };
+
+  const onDeleteTodo = (id) => {
+    const todoIndex = getTodoIndexById(id, todos);
+
+    if (todoIndex !== false) {
+      let newTodos = [...todos];
+      newTodos.splice(todoIndex, 1);
+      console.log(todoIndex);
+      saveTodos(newTodos);
+    }
+  };
+
+  return (
+    <TodoContext.Provider
+      value={{
+        completedTodos,
+        totalTodos,
+        searchedTodos,
+        toggleTodoState,
+        onDeleteTodo,
+        setSearchValue,
+        searchValue,
+        loading,
+        error,
+      }}
+    >
+      {props.children}
+    </TodoContext.Provider>
+  );
+}
+
+export { TodoContext, TodoProvider };

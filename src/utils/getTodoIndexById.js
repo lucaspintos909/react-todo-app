17b32743d9fb8ee27ff290a83f49848b2c8f8f2d
lucaspintos06 <lpintos@ceibal.edu.uy>
2023-01-27 12:25:50 -0300

    refactor: Reubicando utils

--- src/utils/getTodoIndexById.js
+++ src/utils/getTodoIndexById.js
@@ -1,6 +1,6 @@
 export function getTodoIndexById(id, todos) {
   const todoIndex = todos.findIndex((todo) => todo.id === id);
-  console.log(todoIndex);
+
   if (todoIndex === -1) {
     console.error(`Todo de id=${id} no encontrada.`);
     return false;
